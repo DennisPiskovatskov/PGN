7ecce693e513fbabcda070e77aabb76996e074ef
Dennis Piskovatskov <dennis.piskovatskov@javaee.solutions>
2018-06-19 18:44:48 +0200

    Fix path

--- PGN-chessclub.com/scripts/generate-classes-and-show-tree.sh
+++ PGN-chessclub.com/scripts/generate-classes-and-show-tree.sh
@@ -2,4 +2,4 @@
 
 target="../target"
 
-./antlr4-tester.sh PGN.g4 ../src/test/resources/chessclub.com/icc_Zayats72-25percent.pgn parse > $target/tree.antlr4
+./antlr4-tester.sh PGN.g4 ../src/test/resources/chessclub.com/icc_Zayats71.pgn parse > $target/tree.antlr4
